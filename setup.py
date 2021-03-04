2e47c6a1f6217bd35005880ddcc9e7c3c0f282c6
Sidney Radcliffe <sidneyradcliffe@sky.com>
2021-03-04 12:22:22 +0000

    set up for github workflow. and..
    - moved install_requires to use requirements.txt,
    which can be used by github workflow
    
    - moved version to __init__, from setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
+import pathlib
 import setuptools
+import smallpebble
 
-with open("README.md", "r") as f:
-    LONG_DESCRIPTION = f.read()
+requirements = pathlib.Path("requirements.txt").read_text()
+requirements = [r for r in requirements.split("\n") if r]
 
 setuptools.setup(
     name="smallpebble",
-    version="0.1.1",
+    version=smallpebble.__version__,
     author="Sidney Radcliffe",
     author_email="sidneyradcliffe@gmail.com",
     description="Minimal automatic differentiation implementation in Python, NumPy.",
-    long_description=LONG_DESCRIPTION,
+    long_description=pathlib.Path('README.md').read_text(),
     long_description_content_type="text/markdown",
     url="https://github.com/sradc/smallpebble",
     license="Apache License 2.0",
     packages=setuptools.find_packages(),
-    install_requires=["numpy>=1.20.0",],
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
--- smallpebble/__init__.py
+++ smallpebble/__init__.py
@@ -1,1 +1,3 @@
 from smallpebble.smallpebble import *
+
+__version__ = '0.1.1'
