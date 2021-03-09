954dcb159fb4f89a4b6d596d6f60e035fbdb33d3
sradc <sidneyradcliffe@sky.com>
2021-03-09 15:26:52 +0000

    enable pytest arguments, e.g. -x

--- smallpebble/tests/__init__.py
+++ smallpebble/tests/__init__.py
@@ -3,8 +3,13 @@ import pytest
 import smallpebble as sp
 
 
-def run_tests():
-    "Run SmallPebble's tests using pytest."
-    smallpebble_dir = pathlib.Path(sp.__file__).parent
-    smallpebble_dir = str(smallpebble_dir)
-    pytest.main([smallpebble_dir])
+def run_tests(arg_strings=[]):
+    """Run SmallPebble's tests using pytest.
+    
+    arg_strings: a list of pytest arguments, 
+    eg. arg_strings=['-x'] to stop after the first failed test.
+
+    """
+    smallpebble_dir = str(pathlib.Path(sp.__file__).parent)
+    exit_code = pytest.main([smallpebble_dir] + arg_strings)
+    return exit_code
