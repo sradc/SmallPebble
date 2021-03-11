fdf80f2a8c2945db407363e745d68629c18a845d
sradc <sidneyradcliffe@sky.com>
2021-03-11 10:41:38 +0000

    added run_tests_with, for now...
    since don't have a cupy ci environment set up yet.

--- smallpebble/tests/__init__.py
+++ smallpebble/tests/__init__.py
@@ -1,15 +1,32 @@
 import pathlib
 import pytest
 import smallpebble as sp
+import smallpebble.tests.test_smallpebble
 
 
 def run_tests(arg_strings=[]):
-    """Run SmallPebble's tests using pytest.
+    """Run SmallPebble's tests using pytest.main().
     
     arg_strings: a list of pytest arguments, 
     eg. arg_strings=['-x'] to stop after the first failed test.
-
     """
     smallpebble_dir = str(pathlib.Path(sp.__file__).parent)
     exit_code = pytest.main([smallpebble_dir] + arg_strings)
     return exit_code
+
+
+def run_tests_with(array_library):
+    """Set array_library, and then run tests (manually, 
+    by finding `test_` functions in `test_smallpebble.py").    
+    If any test fails, an error is raised.
+    """
+    original_array_library = sp.array_library
+    try:
+        sp.array_library = array_library
+        tests = (
+            f for f in dir(smallpebble.tests.test_smallpebble) if f.startswith("test_")
+        )
+        for test in tests:
+            getattr(smallpebble.tests.test_smallpebble, test)()
+    finally:
+        sp.array_library = original_array_library
