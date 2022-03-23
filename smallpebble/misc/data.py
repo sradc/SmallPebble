0458cb754556a6dfd775eab63f51d73056ae877e
sradc <sidneyradcliffe@sky.com>
2022-03-23 18:03:22 +0000

    refactor: update licenses

--- smallpebble/misc/data.py
+++ smallpebble/misc/data.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The SmallPebble authors
+# Copyright 2022 The SmallPebble Authors, Sidney Radcliffe
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
